ffadd242a093ce46a48b32339658dab2ecf35988
Sohaib Ahmad <sohaibahmad33888237@gmail.com>
2026-04-08 12:07:26 +0500

    Update admin.py

--- Api/admin.py
+++ Api/admin.py
@@ -1,13 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Register
-from .models import Search_data
-from .models import Students
 from .models import Task
 
 admin.site.register(Task)
-admin.site.register(Students)
-admin.site.register(Search_data)
 admin.site.register(Register,UserAdmin)
 
 # Register your models here.
