68cb5b73cb9b2249ac871fb4467836a615585e9a
Sohaib Ahmad <sohaibahmad33888237@gmail.com>
2026-04-08 12:09:50 +0500

    Update serializer.py

--- Api/serializer.py
+++ Api/serializer.py
@@ -2,8 +2,6 @@ from rest_framework import serializers
 from django.contrib.auth.hashers import make_password, check_password
 
 from .models import Register
-from .models import Search_data
-from .models import Students
 from .models import Task
 
 class Register_serializer(serializers.ModelSerializer):
@@ -15,15 +13,6 @@ class Register_serializer(serializers.ModelSerializer):
         validated_data['password']=make_password(validated_data['password'])
         return super().create(validated_data)
 
-class Search_serializer(serializers.ModelSerializer):
-    class Meta:
-        model=Search_data
-        fields='__all__'
-
-class Student_serializer(serializers.ModelSerializer):
-    class Meta:
-        model=Students
-        fields='__all__'
 
 class Task_serializer(serializers.ModelSerializer):
     class Meta:
