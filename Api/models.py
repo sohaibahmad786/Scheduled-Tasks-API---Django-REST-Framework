c500330dd3bc84e5bb7e70881fbbe56997c231b4
Sohaib Ahmad <sohaibahmad33888237@gmail.com>
2026-04-08 12:08:12 +0500

    Update models.py

--- Api/models.py
+++ Api/models.py
@@ -8,24 +8,6 @@ class Register(AbstractUser):
         verbose_name_plural = "Register"
     def __str__(self):
         return self.username
-    
-class Search_data(models.Model):
-    Name=models.CharField()
-    About=models.TextField()
-    created_at=models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.Name
-
-class Students(models.Model):
-    name=models.CharField()
-    rollno=models.IntegerField()
-    city=models.CharField()
-    email=models.EmailField()
-    age=models.IntegerField()
-
-    def __str__(self):
-        return self.name
 
 class Task(models.Model):
     title=models.CharField()
